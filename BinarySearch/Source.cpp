135d9c5848eebdf0792a1d7a95fb0af416a6ede1
MillsWJack <millswjack@googlemail.com>
2018-02-20 12:15:38 +0000

    Setting search value to valid prime number

--- BinarySearch/Source.cpp
+++ BinarySearch/Source.cpp
@@ -28,7 +28,7 @@ int main()
 	vector<int> primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
 		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
 
-	int result = doSearch(primes, 10);
+	int result = doSearch(primes, 73);
 	cout << "Found prime at index " << result << endl;
 	return 0;
 }
